536b7586373e9799dc3b0d0f4e3aa946a494b00b
yaopeng-coder <m201973311@hust.edu.cn>
2019-10-24 13:30:22 +0800

    路由排除，转发和自定义

---
+++ api-gateway/src/test/java/cn/hust/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,14 @@
+package cn.hust.apigateway;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
